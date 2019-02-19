2f5bc6210a6ddeb644ac374dfb09feb0abe2f5e1
leensungzero <brianwoo119@naver.com>
2019-02-19 19:29:17 +0900

    ADD: (User, UserProfile) model 생성

--- scheduler/member/models.py
+++ scheduler/member/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length=30)
+    nickname = models.CharField(max_length=30, unique=True, null=False, blank=False)
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'
+
+    REQUIRED_FIELDS = ['username', 'nickname', ]
+
+
+class UserProfile():
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    LANGUAGE_CLASS = (
+        ('KR', 'Korean'),
+        ('EN', 'English'),
+    )
+
+    birth = models.DateField(null=False, blank=False)
+    language = models.CharField(max_length=2, choices=LANGUAGE_CLASS, default="KR", null=False, blank=True)
