cf450b826d69e11ed9c3909315c8ba8e95bc842d
leensungzero <brianwoo119@naver.com>
2019-03-03 01:30:07 +0900

    Schedule 모델 && API 생성
    ADD: Schedule 모델 생성
    ADD: Schedule migration 파일 생성
    ADD: Schedule 모델 Admin에 등록
    ADD: ScheduleSerializer 생성
    ADD: ScheduleViewSet (CRUD) 생성
    ADD: ScheduleViewSet router에 연결 후 등록

--- scheduler/schedule/admin.py
+++ scheduler/schedule/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Schedule
+
+
+class ScheduleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'registrant', 'start_time', 'state', )
+
+admin.site.register(Schedule)
