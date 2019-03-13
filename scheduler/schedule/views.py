9341cefba15104ffaf9d7ed30d232527f7b4acb0
leensungzero <brianwoo119@naver.com>
2019-03-13 22:51:44 +0900

    Schedule 친구 초대 기능 추가
    ADD: ScheduleSerializer 생성
    ADD: ScheduleViewSet 생성 후 create 뷰 커스터마이징, 일정 초대 action 추가

--- scheduler/schedule/views.py
+++ scheduler/schedule/views.py
@@ -1,13 +1,62 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
 
+from .models import Schedule
 from .serializers import ScheduleSerializer
 
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
+
+User = get_user_model()
 
 class ScheduleViewSet(viewsets.GenericViewSet,
                       mixins.RetrieveModelMixin,
                       mixins.UpdateModelMixin,
                       mixins.CreateModelMixin,
                       mixins.DestroyModelMixin):
+    queryset = Schedule.objects.all()
     serializer_class = ScheduleSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        schedule = serializer.save()
+
+        registrant = serializer.validated_data['registrant']
+        schedule.participants.add(registrant)
+        schedule.save()
+
+        headers = self.get_success_headers(serializer.data)
+        return Response({
+            'status': 201,
+            'message': '일정 생성 완료'
+        }, status=status.HTTP_201_CREATED, headers=headers)
+
+    # Todo: invite action 뷰 생성 (fcm 활용)
+
+    @action(detail=True, methods=['GET'])
+    def join(self, request, pk=None):
+        user = get_user_model()
+        participant_id = request.GET.get('id')
+        try:
+            participant = User.objects.get(id=participant_id)
+            Schedule.objects.get(pk=pk, participants=participant)
+            return Response({
+                'status': 200,
+                'message': '일정에 이미 참가 중인 유저',
+            }, status=status.HTTP_200_OK)
+        except User.DoesNotExist:
+            return Response({
+                'status': 200,
+                'message': '일정에 추가할 user의 id를 입력해주세요',
+            }, status=status.HTTP_200_OK)
+        except Schedule.DoesNotExist:
+            instance = self.get_object()
+            instance.participants.add(participant)
+
+            return Response({
+                'status': 200,
+                'message': '일정에 user 추가',
+            }, status=status.HTTP_200_OK)
