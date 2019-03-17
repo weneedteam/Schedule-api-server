ac4434fd138af307c300487b60d56f2b20952c39
leensungzero <brianwoo119@naver.com>
2019-03-17 16:28:31 +0900

    Holiday api 파싱 스크립트
    ADD: api.py로 api 받아오는 스크립트 추가
    ADD: Holiday 모델 추가
    ADD: Holiday 모델 admin에 등록
    MOD: local_settings의 API_KEY 값 변경

--- scheduler/schedule/admin.py
+++ scheduler/schedule/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 
-from .models import Schedule
+from .models import Schedule, Holiday
 
 
 class ScheduleAdmin(admin.ModelAdmin):
     list_display = ('title', 'registrant', 'start_time', 'state', )
 
+
+class HolidayAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_holiday', 'date', )
+    list_filter = ('name', 'date', )
+
+
 admin.site.register(Schedule)
+admin.site.register(Holiday)
---
+++ scheduler/schedule/api.py
@@ -0,0 +1,42 @@
+import requests, datetime
+from xml.etree import ElementTree
+
+from django.conf import settings
+
+from .models import Holiday
+
+
+class HolidayAPI():
+    def __init__(self):
+        self.holiday_base_url = "http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/{holiday_kinds}?solYear={year}&solMonth={month}&ServiceKey=%s" % settings.API_KEY
+        self.parse_category = ["getHoliDeInfo", "getRestDeInfo", "get24DivisionsInfo", "getSundryDayInfo", ]
+
+    def __call__(self, *args, **kwargs):
+        for category in self.parse_category:
+            self.api_parser(category)
+
+    def api_parser(self, holiday_kinds):
+        for year in range(2015, 2021):
+            year = str(year)
+            for month in range(1, 13):
+                month = str(month).zfill(2)
+
+                response = requests.get(self.holiday_base_url.format(holiday_kinds=holiday_kinds, year=year, month=month))
+                root = ElementTree.fromstring(response.text)
+                items = root.iter('item')
+
+                for item in items:
+                    name = item.find('dateName').text
+                    is_holiday = item.find('isHoliday').text
+                    date = item.find('locdate').text
+
+                    is_holiday = True if is_holiday == 'Y' else False
+
+                    date = datetime.datetime.strptime(date, "%Y%m%d")
+                    self.save_holiday(name, is_holiday, date)
+
+    def save_holiday(self, name, is_holiday, date):
+        Holiday.objects.get_or_create(name=name, is_holiday=is_holiday, date=date)
+
+
+h = HolidayAPI()()
